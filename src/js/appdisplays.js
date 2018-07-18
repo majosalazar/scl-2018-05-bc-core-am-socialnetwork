e037203f60a954b8b2f68a364e11441aa1cb6195
Cote <majovardon@gmail.com>
2018-07-18 09:50:58 -0400

    JS para que sólo se muestren las páginas elegidas del sidebar

--- src/js/appdisplays.js
+++ src/js/appdisplays.js
@@ -1,24 +1,50 @@
+//Funciones para que sólo se muestren las páginas elegidas en el sidebar. 
 wall = () => {
+  logIn.style.display = "none";
   profile.style.display = "none";
+  articlesDisplay.style.display = "none";
+  professionalSupportDisplay.style.display = "none";
+  talksDisplay.style.display = "none";
+  complaintsDisplay.style.display = "none";
   wallDisplay.style.display = "block";
 }
 
 articles = () => {
+  logIn.style.display = "none";
   profile.style.display = "none";
+  professionalSupportDisplay.style.display = "none";
+  talksDisplay.style.display = "none";
+  complaintsDisplay.style.display = "none";
+  wallDisplay.style.display = "none";
   articlesDisplay.style.display = "block";
 }
 
 professionalSupport = () => {
+  logIn.style.display = "none";
   profile.style.display = "none";
+  articlesDisplay.style.display = "none";
+  talksDisplay.style.display = "none";
+  complaintsDisplay.style.display = "none";
+  wallDisplay.style.display = "none";
   professionalSupportDisplay.style.display = "block";
 }
 
 talks = () => {
+  logIn.style.display = "none";
   profile.style.display = "none";
+  articlesDisplay.style.display = "none";
+  professionalSupportDisplay.style.display = "none";
+  complaintsDisplay.style.display = "none";
+  wallDisplay.style.display = "none";
   talksDisplay.style.display = "block";
 }
 
 complaints = () => {
+  logIn.style.display = "none";
   profile.style.display = "none";
+  articlesDisplay.style.display = "none";
+  professionalSupportDisplay.style.display = "none";
+  talksDisplay.style.display = "none";
+  wallDisplay.style.display = "none";
   complaintsDisplay.style.display = "block";
 }
