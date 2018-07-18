79e208dcc027cadb69bb66c9d6540ebf693fb5d8
Cote <majovardon@gmail.com>
2018-07-18 09:02:24 -0400

    Commit previo a merge

---
+++ src/js/appdisplays.js
@@ -0,0 +1,24 @@
+wall = () => {
+  profile.style.display = "none";
+  wallDisplay.style.display = "block";
+}
+
+articles = () => {
+  profile.style.display = "none";
+  articlesDisplay.style.display = "block";
+}
+
+professionalSupport = () => {
+  profile.style.display = "none";
+  professionalSupportDisplay.style.display = "block";
+}
+
+talks = () => {
+  profile.style.display = "none";
+  talksDisplay.style.display = "block";
+}
+
+complaints = () => {
+  profile.style.display = "none";
+  complaintsDisplay.style.display = "block";
+}
--- src/js/appmuro.js
+++ src/js/appmuro.js
@@ -1,5 +1,5 @@
   firebase.database().ref('messages')
-  .limitToLast(5) // Filtro para no obtener todos los mensajes
+  .limitToLast(20) // Filtro para no obtener todos los mensajes
   .once('value')
   .then((messages)=>{
       console.log("Mensajes > "+JSON.stringify(messages));
@@ -11,39 +11,14 @@
 //Acá comenzamos a escuchar por nuevos mensajes usando el evento
 //on child_added
 firebase.database().ref('messages')
-  .limitToLast(1)
+  .limitToLast(20)
   .on('child_added', (newMessage)=>{
       messageContainer.innerHTML += `
           <p>Nombre : ${newMessage.val().creatorName}</p>
           <p>${newMessage.val().text}</p>
       `;
   });
-
-wall = () => {
-  profile.style.display = "none";
-  wallDisplay.style.display = "block";
-}
-
-articles = () => {
-  profile.style.display = "none";
-  articlesDisplay.style.display = "block";
-}
-
-professionalSupport = () => {
-  profile.style.display = "none";
-  professionalSupportDisplay.style.display = "block";
-}
-
-talks = () => {
-  profile.style.display = "none";
-  talksDisplay.style.display = "block";
-}
-
-complaints = () => {
-  profile.style.display = "none";
-  complaintsDisplay.style.display = "block";
-}
-
+  
 function writeNewPost() {
   messageDiv.style.display = "block";
 }
@@ -62,44 +37,3 @@ function sendMessage(){
       text : messageAreaText
   });
 }
-
-/*function writeNewPost(uid, username, picture, title, body) {
-  //Aquí se muestran los datos del post y su autor.
-  const postData = {
-    author: username,
-    uid: uid,
-    body: body,
-    title: title,
-    starCount: 0,
-    authorPic: picture
-  };
-
-  //Con ésto se obtiene una nueva llave para un nuevo mensaje.
-  let newPostKey = firebase.database().ref().child('posts').push().key;
-
-  //Ésta parte es para que el usuario pueda escribir el mensaje simultaneamente con las listas de posts y las listas de posts de otros usuarios.
-  let updates = {};
-  updates['/posts/' + newPostKey] = postData;
-  updates['/user-posts/' + uid + '/' + newPostKey] = postData;
-
-  return firebase.database().ref().update(updates);
-}
-
-//Ésta función lleva la cuenta de las estrellas.
-function toggleStar(postRef, uid) {
-  postRef.transaction(function (post) {
-    if (post) {
-      if (post.stars && post.stars[uid]) {
-        post.starCount--;
-        post.stars[uid] = null;
-      } else {
-        post.starCount++;
-        if (!post.stars) {
-          post.stars = {};
-        }
-        post.stars[uid] = true;
-      }
-    }
-    return post;
-  });
-https://firebase.google.com/docs/database/web/read-and-write?hl=es-419}*/
