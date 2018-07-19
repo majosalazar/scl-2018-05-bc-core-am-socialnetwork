7b02b878910d479531976a773469a20d8fdfd6d6
Cote <majovardon@gmail.com>
2018-07-19 18:49:56 -0400

    Los likes se suben al storage y permanecen en la página

--- src/js/appmuro.js
+++ src/js/appmuro.js
@@ -16,40 +16,52 @@ firebase.database().ref('messages')
     wallMessages.appendChild(messageContainer)
     messageContainer.innerHTML +=
       `<div style = "background-color: #E8910C" class="mb-3">
-      <p class="m-0">Autor: ${newMessage.val().creatorName}</p>
-      <p class="m-0">Mensaje<br>${newMessage.val().text}</p>
-      <button class="btn" style="background-color:#E8910C" dataConvalidacion="${newMessage.key}"  onclick="starPost()">
-      </button>
-        <i class="fas fa-star p-1 pb-1"></i>
-      <button class="btn" style="background-color:#E8910C" dataConvalidacion="${newMessage.key}"  onclick="editPost()">
-        <i class="far fa-edit p-1 pb-1"></i>
-      </button>
-      <button class="btn" style="background-color:#E8910C" dataConvalidacion="${newMessage.key}"  onclick="deletePost()">
-        <i class="fas fa-trash-alt p-1 pb-1"></i>
-      </button> 
+        <p class="m-0">Autor: ${newMessage.val().creatorName}</p>
+        <p class="m-0" id="writeMessage" dataKey="${newMessage.key}" dataMessage="${newMessage.text}">Mensaje<br>${newMessage.val().text}</p>
+          <i class="fas fa-star p-1 pb-1" dataKey="${newMessage.key}" onclick="starPost(event)"><span>${newMessage.val().stars}</span></i>
+          <i class="far fa-edit p-1 pb-1" dataKey="${newMessage.key}" onclick="editPost(event)">
+          </i>
+          <i class="fas fa-trash-alt p-1 pb-1" dataKey="${newMessage.key}" onclick="deletePost(event)"></i>
       </div>`;
   });
 
 //Función para editar el mensaje.
-function editPost() {
-}
+function editPost(event) {
+  messageDiv.style.display = "block";
+  event.stopPropagation();
+  const keyPostToEdit = event.target.getAttribute('dataKey');
+  firebase.database().ref('messages/').child(keyPostToEdit).once('value', function () {
+    let baseMessage = event.target.getAttribute('dataMessage');
+    document.getElementById("messageBox").value = baseMessage.writeMessage;
+  });
+};
+
 
 //Función para eliminar el mensaje.
-function deletePost() {
+function deletePost(event) {
   event.stopPropagation();
   let accept = confirm('¿Está seguro que quiere eliminar el post?');
   if (accept === true) {
-    let keyPostToDelete = event.target.getAttribute('dataConvalidacion');
+    const keyPostToDelete = event.target.getAttribute('dataKey');
     firebase.database().ref('messages/').child(keyPostToDelete).remove();
+    console.log('Se borró el mensaje')
   } else {
     alert('No se borró el mensaje.')
   };
 }
 
 //Función para adicionar estrellas.
-function starPost() {
-
-}
+function starPost(event) {
+  event.stopPropagation();
+  event.target.style.color = 'yellow';
+  const stars = event.target.getAttribute('dataKey');
+  firebase.database().ref('messages/' + stars).once('value', function (post) {
+    let result = (post.val().stars || 0) + 1;
+    firebase.database().ref('messages').child(stars).update({
+      stars: result
+    });
+  });
+};
 
 //Función para mandar mensajes y ocultar el div del mensaje.
 function sendMessage() {
@@ -63,7 +75,8 @@ function sendMessage() {
     firebase.database().ref(`messages/${newMessageKey}`).set({
       creator: currentUser.uid,
       creatorName: currentUser.displayName,
-      text: messageAreaText
+      text: messageAreaText,
+      stars: 0
     });
   } else {
     alert('No puedes subir un mensaje en blanco.')
